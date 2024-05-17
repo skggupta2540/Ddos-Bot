ae9312028c38cb387583230e0b8d75bd8ef40ac5
skggupta2540 <127782470+skggupta2540@users.noreply.github.com>
2024-05-17 17:44:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const { exec } = require('child_process');
 
 // Ganti dengan token bot Telegram Anda
-const token = '7069670027:AAGrM4SX8wAyoFmZpKQQo83qS3aANewmleI';
+const token = '7164751351:AAEDUgr3IhwPN9BaubCY6drE9_Vx7caGQPU';
 
 // Inisialisasi bot dengan token
 const bot = new TelegramBot(token, { polling: true });
